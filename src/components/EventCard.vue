3ad3f2b5a97947ac15dcfa043c3b5a5de2579cc2
Rafal <rafek131@wp.pl>
2025-04-28 21:35:54 +0200

    added events

---
+++ src/components/EventCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+	event: Record<string, any>;
+}>();
+</script>
+
+<template>
+	<div class="event-card">
+		<h2>{{ event.title }}</h2>
+		<span>@{{ event.time }} on {{ event.date }}</span>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.event-card {
+	padding: 20px;
+	width: 250px;
+	cursor: pointer;
+	border: 1px solid #39495c;
+	margin-bottom: 18px;
+}
+
+.event-card:hover {
+	transform: scale(1.01);
+	box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>

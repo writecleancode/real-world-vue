d59025ea58788a013ce6357fa70aa36526d8c927
Rafal <rafek131@wp.pl>
2025-05-04 17:47:59 +0200

    added register button

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+
 const props = defineProps<{
 	event: any;
 }>();
+
+const router = useRouter();
+
+const register = () => {
+	router.push({
+		name: 'event-details',
+	});
+};
 </script>
 
 <template>
 	<p>Register for the event here</p>
+	<button @click="register">Register Me!</button>
 </template>
 
 <style lang="scss" scoped></style>
